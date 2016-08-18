290a8fc8dfdf26bc6aa2d6b06504814baa7cc098
Christopher CHEN <fishdrowned@gmail.com>
2016-08-18 15:31:38 +0800

    fix: class_alias not allowed for built-in classes

--- test-swoole-process.php
+++ test-swoole-process.php
@@ -1,4 +1,17 @@
 <?php
-use Swoole\Process;
 
-Process::wait();
+namespace Swoole {
+    use swoole_process;
+    if (!class_exists('Swoole\Process', false)) {
+
+        class Process extends swoole_process
+        {
+        }
+    }
+}
+
+namespace {
+    use Swoole\Process;
+
+    Process::wait();
+}
